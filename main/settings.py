5320f9ea01e929c355111aaaddbc108494a3001f
vkm2018 <105300183+vkm2018@users.noreply.github.com>
2024-12-04 06:25:57 +0600

    Update settings.py

--- main/settings.py
+++ main/settings.py
@@ -124,8 +124,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-
-STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_DIR = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [STATIC_DIR, ]
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
